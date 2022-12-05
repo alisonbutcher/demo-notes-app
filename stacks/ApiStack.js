0e1f8bbe8c869ae302f671e059a530a60785c66f
Alison Butcher <dev@alisonbutcher.com>
2022-12-06 05:57:39 +1000

    Added Cognito

--- stacks/ApiStack.js
+++ stacks/ApiStack.js
@@ -2,34 +2,35 @@ import { Api, use } from "@serverless-stack/resources";
 import { StorageStack } from "./StorageStack";
 
 export function ApiStack({ stack, app }) {
-  const { table } = use(StorageStack);
+    const { table } = use(StorageStack);
 
-  // Create the API
-  const api = new Api(stack, "Api", {
-    defaults: {
-      function: {
-        permissions: [table],
-        environment: {
-          TABLE_NAME: table.tableName,
+    // Create the API
+    const api = new Api(stack, "Api", {
+        defaults: {
+            authorizer: "iam",
+            function: {
+                permissions: [table],
+                environment: {
+                    TABLE_NAME: table.tableName,
+                },
+            },
         },
-      },
-    },
-    routes: {
-        "GET /notes": "functions/list.main",
-        "POST /notes": "functions/create.main",
-        "GET /notes/{id}": "functions/get.main",
-        "PUT /notes/{id}": "functions/update.main",
-        "DELETE /notes/{id}": "functions/delete.main",
-    },
-  });
+        routes: {
+            "GET /notes": "functions/list.main",
+            "POST /notes": "functions/create.main",
+            "GET /notes/{id}": "functions/get.main",
+            "PUT /notes/{id}": "functions/update.main",
+            "DELETE /notes/{id}": "functions/delete.main",
+        },
+    });
 
-  // Show the API endpoint in the output
-  stack.addOutputs({
-    ApiEndpoint: api.url,
-  });
+    // Show the API endpoint in the output
+    stack.addOutputs({
+        ApiEndpoint: api.url,
+    });
 
-  // Return the API resource
-  return {
-    api,
-  };
+    // Return the API resource
+    return {
+        api,
+    };
 }
